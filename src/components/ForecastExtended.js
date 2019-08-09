cfc6b2bebf184c2aeea2c0e9a03c15c956584bad
walterzambranom <walterzambranom@gmail.com>
2019-08-09 07:23:31 -0500

    Actualización de ForecastExtended.js video 92

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
 import './styles.css';
-
+/*
+const days = [
+	'Lunes',
+	'Martes',
+	'Miercoles',
+	'Jueves',
+	'Viernes',
+];
+const data = {
+	temperature: 10,
+	humidity: 10,
+	weatherState: 'normal',
+	wind: 'normal',
+}
+*/
 class ForecastExtended extends Component {
+
+	constructor() {
+		super();
+		this.state = { forecastData: null }
+	}
+
+	renderForecastItemDays() {
+		return "Render Items";
+		//	return days.map(day => (<ForecastItem weekDay={day} hour={12} data={data}></ForecastItem>))
+	}
+	renderProgress = () => {
+		return <h3>"Cargando prónostico extendido..."</h3>;
+	}
 	render() {
 		const { city } = this.props;
+		const { forecastData } = this.state;
 		return (<div >
 			<h2 className='forecast-title'>Pronóstico extendido para {city}</h2>
+			{forecastData ?
+				this.renderForecastItemDays() :
+				this.renderProgress()}
 		</div>);
 	}
 }
