45fb27394c08514caff9dbdc34676951537dadef
walterzambranom <walterzambranom@gmail.com>
2019-08-22 08:57:02 -0500

    Actualización poniendo el reducer, simplificando el código en App.js y explicación.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import './App.css';
 import { Grid, Col, Row } from 'react-flexbox-grid';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from './components/ForecastExtended';
-import { setCity } from './actions';
 
 const cities = [
 	'Buenos Aires,ar',
@@ -27,12 +24,7 @@ class App extends Component {
 		super();
 		this.state = { city: null };
 	}
-	handleSelectedLocation = city => {
-		this.setState({ city });
-		console.log(`handleSelectedLocation ${city}`);
 
-		this.props.setCity(city);
-	}
 	render() {
 		const { city } = this.state;
 		return (
@@ -48,9 +40,7 @@ class App extends Component {
 				</Row>
 				<Row>
 					<Col xs={12} md={6}>
-						<LocationList cities={cities}
-							onSelectedLocation={this.handleSelectedLocation}>
-						</LocationList>
+						<LocationListContainer cities={cities}></LocationListContainer>
 					</Col>
 					<Col xs={12} md={6}>
 						<Paper elevation={4}>
@@ -72,15 +62,8 @@ class App extends Component {
 	}
 
 }
-App.propTypes = {
-	setCity: PropTypes.func.isRequired,
 
-}
-const mapDispatchToPropsActions = dispatch => ({
-	setCity: value => dispatch(setCity(value))
-});
-
-export default connect(null, mapDispatchToPropsActions)(App);
+export default App;
 
 
 
